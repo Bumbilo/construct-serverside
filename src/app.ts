fb168dffd5b4ab9efbae8e5073dae700eb86159e
Bumbilo <dimagolyak@gmail.com>
2019-05-26 23:05:52 +0300

    change for typescript

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+// // const path = require('path');
+// // const express = require('express');
+// // const bodyParser = require('body-parser');
+// import path from  'path';
+// import bodyParser from 'body-parser';
+
+
+const app = express();
+
+// // app.use(bodyParser.urlencoded({ extended: false }));
+// // app.use(express.static(path.join(__dirname, 'public')));
+
+// // app.set('view engine', 'ejs');
+// // app.set('views', 'views');
+
+// // const authRoutes = require('./routes/auth')
+// // app.use(authRoutes);
+
+// app.listen(8080);
+
+export default app;
